d899b504b39c101b842c8cf38564650c21b7dc59
Filipe Laíns <lains@riseup.net>
2021-01-13 18:46:45 +0000

    fox: add custom process pool to be able to get record the output
    Signed-off-by: Filipe Laíns <lains@riseup.net>

--- fox/__init__.py
+++ fox/__init__.py
@@ -1,9 +1,13 @@
 # SPDX-License-Identifier: EUPL-1.2
 
 import collections
-import concurrent.futures
+import io
+import multiprocessing
+import multiprocessing.connection
+import sys
 
-from typing import Any, Callable, List
+from collections.abc import Iterable
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 
 __version__ = '0.0.1'
@@ -21,14 +25,78 @@ def _sequencial_executor(tasks: List[_Task]) -> None:
         task.func()
 
 
+class _ProcessPool:
+    def __init__(self) -> None:
+        self._pool: Dict[str, Tuple[
+            multiprocessing.Process,
+            multiprocessing.connection.Connection,
+        ]] = {}
+        self._ready: multiprocessing.Queue[str] = multiprocessing.Queue()
+
+    def submit(
+        self,
+        name: str,
+        func: Callable[..., None],
+        args: Optional[Tuple[Any, ...]] = None,
+        kwargs: Optional[Dict[str, Any]] = None,
+    ) -> None:
+        recv_out, send_out = multiprocessing.Pipe()
+        process = multiprocessing.Process(
+            target=self._process_entry,
+            args=(name, func, args or [], kwargs or {}, send_out),
+        )
+        process.start()
+        self._pool[name] = process, recv_out
+
+    def _process_entry(
+        self,
+        name: str,
+        func: Callable[..., None],
+        args: Tuple[Any, ...],
+        kwargs: Dict[str, Any],
+        send_out: multiprocessing.connection.Connection,
+    ) -> None:
+        with io.StringIO() as out:
+            sys.stdout, sys.stderr = out, out
+            try:
+                func(*args, **kwargs)
+            finally:
+                send_out.send(out.getvalue())
+                self._ready.put(name)
+
+    def as_completed(self) -> Iterable[Tuple[
+        str,
+        multiprocessing.Process,
+        multiprocessing.connection.Connection,
+    ]]:
+        while True:
+            name = self._ready.get()
+
+            process, recv_out = self._pool[name]
+            yield name, process, recv_out.recv()
+
+            del self._pool[name]
+            if not self._pool:  # no pending tasks
+                break
+
+
 def _parallel_executor(tasks: List[_Task]) -> None:
     import tqdm
 
-    with tqdm.tqdm(total=len(tasks)) as progress:
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            futures = [executor.submit(task.func) for task in tasks]
-            for future in concurrent.futures.as_completed(futures):
-                progress.update(1)
+    with tqdm.tqdm(total=len(tasks), desc='Running tasks', bar_format='{l_bar}{bar}| ') as progress:
+        pool = _ProcessPool()
+        for task in tasks:
+            pool.submit(task.name, task.func)
+
+        # wait for tasks to complete
+        for name, process, out in pool.as_completed():
+            process.join()
+            if process.exitcode:
+                progress.write(f'error in `{name}`!')
+                progress.write(out, end='')
+            else:
+                progress.write(f'done: {name} ({process.exitcode})')
+            progress.update(1)
 
 
 _tasks: List[_Task] = []
